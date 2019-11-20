8f6f47c00b7c6be7ca290df9eb55f50fd52d08e8
tongyijia <tongyijialinux@163.com>
2019-11-20 21:35:43 +0800

    2019-11-20

---
+++ C1-W4/dnn_utils.py
@@ -0,0 +1,41 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+import numpy as np
+
+def sigmoid(Z):
+    
+    A = 1 / (1 + np.exp(Z))
+    cache = Z
+    
+    return A, cache
+
+def sigmoid_backward(dA, cache):
+    
+    Z = cache
+    
+    s = 1 / (1 + np.exp(-Z))
+    dZ = dA * s * (1 - s)
+    
+    return dZ
+
+def relu(Z):
+    
+    A = np.maximum(0, Z)
+    
+    cache = Z
+    
+    return A, cache
+
+def relu_backward(dA, cache):
+    
+    Z = cache
+    dZ = np.array(dA, copy = True)
+    
+    dZ[Z <= 0] = 0
+    
+    return dZ
+
